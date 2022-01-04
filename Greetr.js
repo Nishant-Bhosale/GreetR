182b2b7a5e690572b204ee16803d37f95f574028
Nishant Bhosale <nishantbhosale244@gmail.com>
2022-01-04 16:46:28 +0530

    Changed Prototype Reference

--- Greetr.js
+++ Greetr.js
@@ -1,1 +1,17 @@
-(function (global, $) {})(window, jQuery);
+(function (global, $) {
+	var Greetr = function (firstName, lastName, language) {
+		return new Greetr.init(firstName, lastName, language);
+	};
+
+	Greetr.prototype = {};
+
+	Greetr.init = function (firstName, lastName, language) {
+		let self = this;
+
+		self.firstName = firstName || "Your FirstName";
+		self.lastName = lastName || "Your LastName";
+		self.language = language || "en";
+	};
+
+	Greetr.init.prototype = Greetr.prototype;
+})(window, jQuery);
