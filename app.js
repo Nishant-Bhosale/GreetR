e1e3875ab982c81d4a3699d61999a6ce351c8dd7
Nishant Bhosale <nishantbhosale244@gmail.com>
2022-01-07 16:29:21 +0530

    Added more functions

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
-const g = $G("Nishant", "Bhosale", "en=e");
-console.log(g);
+const button = document.getElementById("login");
+const loginDiv = document.getElementById("lang");
+
+button.addEventListener("click", function (e) {
+	let g = $G("Nishant", "Bhosale", loginDiv.value);
+	console.log(loginDiv.value);
+	g.changeText("greeting", true);
+});
 
 //Without chaining
 // g.greet(true);
 // g.log();
 
 //With chaining
-// g.setLanguage("es").greet(false).setLanguage("en").log();
+// g.setLanguage("es").greet(false).setLanguage("en").setLanguage("es").log();
 // g.changeText("greeting", true);
